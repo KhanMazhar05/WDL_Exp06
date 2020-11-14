d05c08b566f4c61ea37065be2df9a13f4fb270d6
Mazhar <mazharsultankhan10@gmail.com>
2020-11-14 16:39:25 +0530

    connection Modified

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php 
 
-$servername ="localhost";
-$username = "root";
-$password = "";
-$database = "chachastore";
+$servername ="sql202.epizy.com";
+$username = "epiz_27192045";
+$password = "DrGIbb9d7NtybBA";
+$database = "epiz_27192045_chachastore";
 
 $conn = mysqli_connect($servername,$username,$password,$database);
 
